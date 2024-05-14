eb1a8340a5998452cd2ad48757e12d5c1ed3229b
still-jansons <81186072+still-jansons@users.noreply.github.com>
2024-05-14 18:31:47 +0300

    Improve LogoElement.tsx shadow

--- app/components/LogoElement.tsx
+++ app/components/LogoElement.tsx
@@ -17,11 +17,22 @@ export default function LogoElement({
           target="_blank"
           rel="noopener noreferrer"
         >
-          <div
-            className={
-              "logo-shadow flex h-36 w-36 items-center justify-center overflow-hidden rounded-full bg-white p-1"
-            }
-          >
+          <div className="logo-shadow relative h-36 w-36 rounded-full bg-white">
+            <div className="flex h-full w-full items-center justify-center p-1">
+              <Image
+                src={blok.image.filename as string}
+                alt={blok.image.alt as string}
+                width={150}
+                height={150}
+                quality={100}
+                className="h-full w-full rounded-full object-contain"
+              />
+            </div>
+          </div>
+        </a>
+      ) : (
+        <div className="logo-shadow relative h-36 w-36 rounded-full bg-white">
+          <div className="flex h-full w-full items-center justify-center p-1">
             <Image
               src={blok.image.filename as string}
               alt={blok.image.alt as string}
@@ -31,21 +42,6 @@ export default function LogoElement({
               className="h-full w-full rounded-full object-contain"
             />
           </div>
-        </a>
-      ) : (
-        <div
-          className={
-            "logo-shadow flex h-36 w-36 items-center justify-center overflow-hidden rounded-full bg-white p-1"
-          }
-        >
-          <Image
-            src={blok.image.filename as string}
-            alt={blok.image.alt as string}
-            width={150}
-            height={150}
-            quality={100}
-            className="h-full w-full rounded-full object-contain"
-          />
         </div>
       )}
     </>
