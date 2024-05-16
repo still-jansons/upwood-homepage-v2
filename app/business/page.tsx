20a4462d05417c9e071b96afa4aa93be38cd08fd
still-jansons <81186072+still-jansons@users.noreply.github.com>
2024-05-16 17:30:43 +0300

    Try getData()

--- app/business/page.tsx
+++ app/business/page.tsx
@@ -7,12 +7,12 @@ import {
 import { initStoryblok } from "@/src/storyblok";
 
 export default async function Business() {
-  const { data } = await fetchData();
+  const { data } = await getData();
 
   return <StoryblokComponent blok={data.story.content} />;
 }
 
-export async function fetchData() {
+export async function getData() {
   initStoryblok();
   let sbParams: ISbStoriesParams = { version: "draft" };
 
