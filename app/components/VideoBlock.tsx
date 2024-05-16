e84141f3d399166ad5a48a6b902b7f5d3d0170b6
still-jansons <81186072+still-jansons@users.noreply.github.com>
2024-05-16 19:00:07 +0300

    Lazy load youtube video

--- app/components/VideoBlock.tsx
+++ app/components/VideoBlock.tsx
@@ -1,4 +1,5 @@
 import { storyblokEditable } from "@storyblok/react";
+import { YouTubeEmbed } from "@next/third-parties/google";
 
 export default function VideoBlock({
   blok,
@@ -12,17 +13,14 @@ export default function VideoBlock({
       {...storyblokEditable(blok)}
       className="mx-auto flex w-full max-w-5xl flex-col items-center gap-8"
     >
-      <iframe
-        className="mx-auto aspect-video w-full overflow-hidden rounded-3xl drop-shadow-xl"
-        width="560"
-        height="auto"
-        src={blok.videoEmbed}
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        frameBorder="0"
-        allowFullScreen
-        loading="lazy"
-      ></iframe>
+      <div className={"w-full"}>
+        <YouTubeEmbed
+          videoid={blok.videoEmbed}
+          style={
+            "max-width: 100%; border-radius: 1.5rem; height:100%; filter: drop-shadow(0 20px 13px rgb(0 0 0 / 0.03)) drop-shadow(0 8px 5px rgb(0 0 0 / 0.08));"
+          }
+        />
+      </div>
     </div>
   );
 }
